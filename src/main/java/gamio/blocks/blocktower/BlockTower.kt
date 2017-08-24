5db6a2be98b1e934e0334e10e76f9723ca413718
wintu <kugateruhisa@gmail.com>
2017-08-24 13:14:51 +0900

    指摘点修正

--- src/main/java/gamio/blocks/blocktower/BlockTower.kt
+++ src/main/java/gamio/blocks/blocktower/BlockTower.kt
@@ -12,7 +12,9 @@ import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.server.MapInitializeEvent
 import org.bukkit.inventory.ItemStack
+import org.bukkit.map.*
 import org.bukkit.scheduler.BukkitTask
 import org.bukkit.scoreboard.DisplaySlot
 import org.bukkit.scoreboard.Objective
@@ -35,6 +37,8 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
     var world: World? = null
 
     var time: Score? = null
+
+    var isStart: Boolean = false
 
     var counter: Int = 1800
     var task: BukkitTask? = null
@@ -75,13 +79,14 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
         objective!!.setDisplayName("制限時間(秒)")
         objective!!.displaySlot = DisplaySlot.SIDEBAR
         time = objective!!.getScore("残り秒数:")
-        time!!.score = 0
+        time!!.score = counter
         Bukkit.getPluginManager().registerEvents(this,this);
 
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+
     }
 
     override fun onCommand(sender: CommandSender?, command: Command?, label: String?, args: Array<out String>?): Boolean {
@@ -95,15 +100,23 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
                     }
                     when (args?.get(0)) {
                         "start" -> {
+                            val stonePicx = ItemStack(Material.STONE_PICKAXE)
+                            val itemData = stonePicx.itemMeta
+                            itemData.displayName = "すっごーい石ピッケル"
+                            itemData.isUnbreakable = true
+                            stonePicx.itemMeta = itemData
                             for (p in Bukkit.getServer().onlinePlayers) {
                                 p.inventory.clear()
-                                p.inventory.addItem(ItemStack(Material.STONE_PICKAXE, 3))
+                                p.inventory.addItem(stonePicx)
                                 p.inventory.addItem(ItemStack(Material.WOOD, 10))
+                                p.inventory.addItem(ItemStack(Material.COOKED_BEEF, 10))
                                 p.inventory.addItem(ItemStack(Material.TORCH, 64))
+                                p.inventory.addItem(ItemStack(Material.EMPTY_MAP))
                                 p.gameMode = GameMode.SURVIVAL
-                                p.sendTitle("試合開始", "制限時間は30分です")
+                                p.sendTitle("試合開始", "制限時間は" + (counter / 60) + "分です")
                             }
                             task = Bukkit.getScheduler().runTaskTimer(this, CountDownTimer(), 0, 20)
+                            isStart = true
                             return true
                         }
                         "init" -> {
@@ -111,6 +124,15 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
                             redLoc = Location(world, (initLoc!!.x - 4), (initLoc!!.y - 1), initLoc!!.z)
                             blueLoc = Location(world, (initLoc!!.x + 4), (initLoc!!.y - 1), initLoc!!.z)
                             InitWorld()
+                            return true
+                        }
+                        "time" -> {
+                            if (args?.lastIndex != 1) {
+                                p.sendMessage("指定時間を入力してください。")
+                                return true
+                            }
+                            counter = args?.get(1).toInt()
+                            time!!.score = counter
                             return true
                         }
                         "result" -> {
@@ -128,6 +150,17 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
                                 p.sendTitle(resultMessage, "おめでとうございます！")
                             }
                             Bukkit.getServer().broadcastMessage("総ポイント数 Red:" + redResult + "    Blue:" + blueResult)
+                            return true
+                        }
+                        "stop" -> {
+                            isStart = false
+                            task!!.cancel()
+                            counter = 1800
+                            for (p in Bukkit.getServer().onlinePlayers) {
+                                p.teleport(initLoc)
+                                p.gameMode = GameMode.SPECTATOR
+                                p.sendTitle("試合中止", "お疲れさまでした。")
+                            }
                             return true
                         }
                         else -> {
@@ -172,6 +205,9 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
          world!!.getBlockAt(blueLoc!!.x.toInt(), blueLoc!!.y.toInt(), blueLoc!!.z.toInt() - 1).type = WOOL_BLUE.itemType
          world!!.getBlockAt(blueLoc!!.x.toInt(), blueLoc!!.y.toInt(), blueLoc!!.z.toInt() - 1).data = WOOL_BLUE.data
 
+         world!!.getBlockAt(redLoc!!.x.toInt(), redLoc!!.y.toInt(), redLoc!!.z.toInt()).type = Material.WOOL
+         world!!.getBlockAt(blueLoc!!.x.toInt(), blueLoc!!.y.toInt(), blueLoc!!.z.toInt()).type = Material.WOOL
+
          val y = (redLoc!!.y.toInt() + 1)
          for (i in y..256) {
              world!!.getBlockAt(redLoc!!.x.toInt(), i, redLoc!!.z.toInt() + 2).type = Material.STONE
@@ -208,6 +244,8 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
     fun CountDownTimer()  : Runnable {
         var r: Runnable = Runnable {
             if (counter == 0){
+                time!!.score = counter
+                isStart = false
                 task!!.cancel()
                 counter = 1800
                 for (p in Bukkit.getServer().onlinePlayers) {
@@ -224,17 +262,52 @@ class BlockTower : JavaPlugin(), Listener, CommandExecutor {
     }
 
     @EventHandler
-    public fun exampleEvent(event: PlayerJoinEvent){
+    public fun joinEvent(event: PlayerJoinEvent){
         val p = event.player
-        p.inventory.clear()
-        p.gameMode = GameMode.ADVENTURE
-        if (teamRed!!.entries.size > teamBlue!!.entries.size){
-            teamBlue?.addEntry(p.name)
-        } else if (teamRed!!.entries.size == teamBlue!!.entries.size) {
-            teamRed?.addEntry(p.name)
+        if (isStart) {
+            if (teamRed!!.hasEntry(p.name) || teamBlue!!.hasEntry(p.name)) { return }
+            p.gameMode = GameMode.SPECTATOR
+            p.sendTitle("試合中です", "観戦モードに切り替えました")
         } else {
-            teamRed?.addEntry(p.name)
+            p.inventory.clear()
+            if (initLoc != null){
+                p.teleport(initLoc)
+            }
+            p.gameMode = GameMode.ADVENTURE
+            if (teamRed!!.entries.size > teamBlue!!.entries.size){
+                teamBlue?.addEntry(p.name)
+            } else if (teamRed!!.entries.size == teamBlue!!.entries.size) {
+                teamRed?.addEntry(p.name)
+            } else {
+                teamRed?.addEntry(p.name)
+            }
         }
+
+    }
+
+    @EventHandler
+    public fun initMap(event: MapInitializeEvent){
+        if (initLoc == null){
+            Bukkit.getServer().broadcastMessage("活動拠点をセットしてください。")
+            return
+        }
+        val mapview = event.map
+        mapview.isUnlimitedTracking = true
+        mapview.centerX = initLoc!!.x.toInt()
+        mapview.centerZ = initLoc!!.z.toInt()
+        mapview.scale = MapView.Scale.CLOSE
+        mapview.world = world
+        mapview.addRenderer(PointRender())
     }
 
 }
+
+class PointRender: MapRenderer() {
+    public override fun render(map: MapView?, canvas: MapCanvas?, player: Player?) {
+        var s = "×"
+        val width = MinecraftFont.Font.getWidth(s)
+        val height = MinecraftFont.Font.height
+        canvas!!.drawText(64 - (width / 2), 64 - (height / 2), MinecraftFont.Font, s)
+    }
+}
+
